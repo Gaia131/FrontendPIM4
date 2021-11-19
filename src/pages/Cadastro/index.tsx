f893777307a496bd7022c1906203f22afbd41588
Leticia <leticia.sueli1998@gmail.com>
2021-11-18 22:56:05 -0300

    versao58

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -56,59 +56,73 @@ const Cadastro: React.FC = () => {
             <div className="container">
                 <br />
                 <div className="titulo">
-                    <h1>Cadastro de usuario</h1>
+                    <h1>Cadastro de usuário</h1>
                 </div>
                 <br />
+                <table>
                 <div className="container">
                     <Form onSubmit={onSubmit}>
-                        <div className = "nome" >
-                            <Form.Label>Nome</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="nome"
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
-                        </div>
-    
-                        <div className = "cpf"> 
-                            <Form.Label>CPF</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="cpf"
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
-                        </div>
-                        
-                        <Form.Group>
-                            <Form.Label>Telefone</Form.Label>
-                            <Form.Control
-                                className = "tel"
-                                type="text"
-                                name="telefone"
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
-                        </Form.Group>
-                        
-                        <Form.Group>
-                            <Form.Label>E-mail</Form.Label>
-                            <Form.Control
-                                className = "email"
-                                type="text"
-                                name="email"
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control
-                                className = "senha"
-                                type="password"
-                                name="senha"
-                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
-                        </Form.Group>
-                        <div className = 'botoes'>
-                        <Button type="submit" className ="btn btn-primary" onClick={SalvareVoltar} >Salvar</Button>
-                        <button type="button" className ="btn btn-warning" onClick={Cancelar} >Cancelar</button>
-                        </div>
+                        <tr>
+                            <div className = "nome" >
+                                <label className="lnome">Nome</label>
+                                <Form.Control
+                                    type="text"
+                                    name="nome"
+                                    onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
+                            </div>
+                        </tr>
+                        <tr>
+                                <td>
+                                    <div className = "cpf" >
+                                    <label className="lcpf">CPF</label>
+                                    <Form.Control
+                                        type="text"
+                                        name="cpf"
+                                        onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className = "telefone" >
+                                    <label className="ltelefone">Telefone</label>
+                                    <Form.Control
+                                        className = "tel"
+                                        type="text"
+                                        name="telefone"
+                                        onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
+                                    </div>
+                                </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className = "email" >
+                                <label className="lemail">E-mail</label>
+                                <Form.Control
+                                    className = "email"
+                                    type="text"
+                                    name="email"
+                                    onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
+                                </div>
+                            </td>
+                            <td>
+                                <div className = "senha" >
+                                <label className="lsenha">Senha</label>
+                                <Form.Control
+                                    className = "senha"
+                                    type="password"
+                                    name="senha"
+                                    onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}/>
+                                </div>
+                            </td>
+                        </tr>
+                        <br/>
+                        <br/>
                     </Form>
+                    <div className = 'botoes'>
+                        <Button type="submit" className ="btn btn-primary" onClick={SalvareVoltar} >Salvar</Button>
+                        <Button type="submit" className ="btn btn-warning" onClick={Cancelar} >Cancelar</Button>
+                    </div>
                 </div>
+                </table>
             </div>
         <br/>
         <br/>
